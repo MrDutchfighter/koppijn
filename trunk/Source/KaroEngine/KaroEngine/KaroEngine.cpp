6d9850c4517a4d2b5a2e9c616655c2027bb1404e
khai.pham1@gmail.com <khai.pham1@gmail.com@fb384490-38b7-be2d-62f0-9a43054d88e4>
2011-04-18 10:29:41 +0000

    engine domove enzo

--- trunk/Source/KaroEngine/KaroEngine/KaroEngine.cpp
+++ trunk/Source/KaroEngine/KaroEngine/KaroEngine.cpp
@@ -6,29 +6,66 @@
 
 KaroEngine::KaroEngine(void)
 {
-	board = new int[255];
-	this->turn = Black;
-	int gamestate = Insertion; // 0 is insertionstate 1 is gameplaystate 2 is gameover
+	board = new FieldTile[BOARDWIDTH * BOARDWIDTH];
+	this->turn = WHITE;
+	int gamestate = INSERTION; // 0 is insertionstate 1 is gameplaystate 2 is gameover
+
+	for(int i = 0; i < BOARDWIDTH * BOARDWIDTH ; i ++ )
+		board[i] = EMPTY;
+
+	for(int j = 4; j < 8; j++)
+		for( int k = 5; k < 10; k++ )
+			board[j*BOARDWIDTH+k] = TILE;
 }
 
 KaroEngine::~KaroEngine(void)
 {
 
 }
-void KaroEngine::DoMove()
+void KaroEngine::DoMove(int from, int to)
 {
-	
+	if(IsTile(to))
+	{
+
+		board[to] = board[from];
+	}
 }
 
+bool KaroEngine::IsValidMove(int from, int to)
+{
+	bool valid = false;
+	// grenzende move
+	int move = from - to;
+	if(move == -1 || move == 1 || move == -BOARDWIDTH+1 || move == -BOARDWIDTH || move == -BOARDWIDTH-1 || move == BOARDWIDTH-1 || move == BOARDWIDTH || move == BOARDWIDTH+1 )
+	{
+		if(IsTile(to))
+			valid = true;
+	}
+	// jump move
+	else if(move == -2 || move == 2 || move == -(2*BOARDWIDTH)+1 || move == -2*BOARDWIDTH || move == -(2*BOARDWIDTH)-1 || move == (2*BOARDWIDTH)-1 || move == 2*BOARDWIDTH || move == (2*BOARDWIDTH)+1 )
+	{
+		if(IsTile(to))
+		{
+			FieldTile tempField = board[((from - to) / 2) + to];
+			if ( tempField == WHITEUNMARKED || tempField == WHITEUNMARKED || tempField == WHITEUNMARKED || tempField == WHITEUNMARKED )
+			{
+				valid = true;
+			}
+		}
+	}
+
+	return valid;
+
+}
 void KaroEngine::UndoMove()
 {
 }
 
 bool KaroEngine::IsTileEmpty(int tile)
 {
-	return true;
+	return board[tile] == EMPTY;
 }
 bool KaroEngine::IsTile(int tile)
 {
-	return true;
+	return board[tile] == TILE;
 }
