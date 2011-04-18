29488f3223a9aa949ee3cc1bda545e8872d57125
lucas.pekel@gmail.com <lucas.pekel@gmail.com@fb384490-38b7-be2d-62f0-9a43054d88e4>
2011-04-18 10:20:32 +0000

    koppijn 2.0 c# basisgui

--- trunk/Source/KaroTestGUI/KaroTestGUI/Form1.Designer.cs
+++ trunk/Source/KaroTestGUI/KaroTestGUI/Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblEvalScore = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDoMove = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(376, 376);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // lblEvalScore
+            // 
+            this.lblEvalScore.AutoSize = true;
+            this.lblEvalScore.Location = new System.Drawing.Point(12, 452);
+            this.lblEvalScore.Name = "lblEvalScore";
+            this.lblEvalScore.Size = new System.Drawing.Size(60, 13);
+            this.lblEvalScore.TabIndex = 1;
+            this.lblEvalScore.Text = "Eval score:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(709, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Q";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnDoMove
+            // 
+            this.btnDoMove.Location = new System.Drawing.Point(557, 51);
+            this.btnDoMove.Name = "btnDoMove";
+            this.btnDoMove.Size = new System.Drawing.Size(140, 23);
+            this.btnDoMove.TabIndex = 3;
+            this.btnDoMove.Text = "Compute Computer Move";
+            this.btnDoMove.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(557, 174);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(140, 23);
+            this.btn.TabIndex = 4;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(709, 474);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btnDoMove);
+            this.Controls.Add(this.lblEvalScore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(725, 512);
+            this.MinimumSize = new System.Drawing.Size(725, 512);
+            this.Name = "Form1";
+            this.Text = "Karo Test GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblEvalScore;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnDoMove;
+        private System.Windows.Forms.Button btn;
+
     }
 }
 
--- trunk/Source/KaroTestGUI/KaroTestGUI/Form1.cs
+++ trunk/Source/KaroTestGUI/KaroTestGUI/Form1.cs
@@ -9,11 +9,143 @@ using System.Windows.Forms;
 
 namespace KaroTestGUI
 {
+
     public partial class Form1 : Form
     {
+
+        Pen penBlack;
+        Pen penGray;
+        Brush   brushBlack;
+        Brush   brushWhite;
+        Brush   brushRed;
+        Brush   brushBlue;
+        int     boxSize = 25;
+        bool    gameOver=false; 
+
+
+
+        Point clickedFirst;
+        Point clickedSecond;
+        public enum BOARDTILES { EMPTY,TILE, MOVEABLETILE,WHITEMARKED,WHITEUNMARKED,REDMARKED,REDUNMARKED }
+
+        BOARDTILES[] board;
+
         public Form1()
-        {
+        {            
+            penBlack        = Pens.Black;
+
+            penGray = new Pen(Color.Gray, 2);    
+
+            brushBlack      = Brushes.Black;            
+            brushRed        = Brushes.Red;
+            brushWhite      = Brushes.White;
+            brushBlue       = Brushes.Blue;
+
+            clickedFirst    = new Point(-1, -1);
+            clickedSecond   = new Point(-1, -1);
+
+            board = new BOARDTILES[225];
+            for (int y = 0; y < 15; y++) {
+                for (int x = 0; x < 15; x++) {
+                    board[(y * 15) + x] = BOARDTILES.EMPTY;
+                }
+            }
+
+            for (int y = 4; y < 8; y++) {
+                for (int x = 5; x < 10; x++) {                    
+                    board[(y * 15) + x] = BOARDTILES.TILE;
+                }
+            }
+
+
+
             InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;            
+            for (int y = 0; y < 15; y++) {
+                for (int x = 0; x < 15; x++) {
+                    
+
+                    //Draw the board
+                    if (board[(y * 15) + x] != BOARDTILES.EMPTY ) {
+                        g.FillRectangle(brushBlack, x * boxSize, y * boxSize, boxSize, boxSize);
+                    }
+                    //draw the 'selected' tiles
+                    if (x == clickedFirst.X && y == clickedFirst.Y)
+                    {
+                        g.FillRectangle(brushBlue, x * boxSize, y * boxSize, boxSize, boxSize);
+                    }
+                    if (x == clickedSecond.X && y == clickedSecond.Y)
+                    {
+                        g.FillRectangle(brushBlue, x * boxSize, y * boxSize, boxSize, boxSize);
+                    }
+
+                    //check what kind of tiles, pawns etc are on the board.
+                    switch (board[(y * 15) + x]) { 
+                        case BOARDTILES.EMPTY:
+                            break;
+                        case BOARDTILES.TILE:
+                            break;
+                        case BOARDTILES.MOVEABLETILE:
+                            break;
+                        case BOARDTILES.REDUNMARKED:
+                            g.FillEllipse(brushRed, x * boxSize+1, y * boxSize+1, boxSize-2, boxSize-2);                            
+                            break;
+                        case BOARDTILES.REDMARKED:
+                            g.FillEllipse(brushRed, x * boxSize + 1, y * boxSize + 1, boxSize - 2, boxSize -2 );
+                            g.DrawEllipse(penGray, x * boxSize + 5, y * boxSize + 5, boxSize - 10, boxSize - 10);
+                            break;
+                        case BOARDTILES.WHITEUNMARKED:
+                            g.FillEllipse(brushWhite, x * boxSize + 1, y * boxSize + 1, boxSize - 2, boxSize - 2);
+                            
+                            break;
+                        case BOARDTILES.WHITEMARKED:
+                            g.FillEllipse(brushWhite, x * boxSize + 1, y * boxSize + 1, boxSize - 2, boxSize - 2);
+                            g.DrawEllipse(penGray, x * boxSize + 5, y * boxSize + 5, boxSize - 10, boxSize - 10);
+                            break;                    
+                    }
+
+                    //Draw a grid
+                    g.DrawRectangle(Pens.Gray, x * boxSize, y * boxSize, boxSize, boxSize);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!this.gameOver)
+            {
+                if (clickedFirst.X == -1)
+                {
+                    clickedFirst.X = (e.X - 1) / this.boxSize;
+                    clickedFirst.Y = (e.Y - 1) / this.boxSize;
+                    board[(clickedFirst.Y * 15) + clickedFirst.X] = BOARDTILES.REDMARKED;
+                }
+                else if (clickedSecond.X == -1)
+                {
+                    clickedSecond.X = (e.X - 1) / this.boxSize;
+                    clickedSecond.Y = (e.Y - 1) / this.boxSize;
+                    board[(clickedSecond.Y * 15) + clickedSecond.X] = BOARDTILES.WHITEMARKED;
+                }
+                else {
+                    clickedFirst = new Point(-1, -1);
+                    clickedSecond = new Point(-1, -1);
+                }
+            }
+            pictureBox1.Invalidate();
         }
     }
 }
