55764e7dfce18358302144a2d35b30065ab79111
ilianspoor <ilianspoor@gmail.com>
2011-04-27 08:24:11 +0000

    Move expanded met tiles

--- Source/KaroTestGUI/KaroEngine/Move.h
+++ Source/KaroTestGUI/KaroEngine/Move.h
@@ -4,9 +4,11 @@ public class Move
 public:
 	int positionFrom;
 	int positionTo;
+	int tileFrom;
+	int tileTo;
 	int score;
 	Move(void);
 	Move(int to);
-	Move(int from, int to);
+	Move(int from, int to, int tileFrom, int tileTo);
 };
 
