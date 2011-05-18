1788cf4d40dd58e59683dd805ec73611d4f5f9b2
khai.pham1@gmail.com <khai.pham1@gmail.com@fb384490-38b7-be2d-62f0-9a43054d88e4>
2011-05-18 10:24:01 +0000

    menu

---
+++ trunk/Source/KaroTestGUI/KaroXNA/KaroXNA/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KaroXNA
+{
+    public class MenuItem
+    {
+        private String menuName;
+
+        public String MenuName
+        {
+            get { return menuName; }
+            set { menuName = value; }
+        }
+
+        public MenuItem(String name)
+        {
+            menuName = name;
+        }
+    }
+}
