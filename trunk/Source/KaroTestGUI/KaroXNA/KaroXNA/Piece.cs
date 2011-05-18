37d83309f384011c7241040632f783cff4377f08
alexsteenbruggen@gmail.com <alexsteenbruggen@gmail.com@fb384490-38b7-be2d-62f0-9a43054d88e4>
2011-05-18 09:00:00 +0000

    poging 2

---
+++ trunk/Source/KaroTestGUI/KaroXNA/KaroXNA/Piece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Graphics;
+namespace KaroXNA
+{
+    class Piece
+    {
+        public Model PieceModel { get; set; }
+        public Matrix PieceMatrix { get; set; }
+        public bool IsVisible { get; set; }
+
+        public Piece(Model model, bool visible)
+        {
+            PieceModel = model;
+            IsVisible = visible;
+        }
+    }
+}
