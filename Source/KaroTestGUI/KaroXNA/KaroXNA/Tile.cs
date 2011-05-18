11c39b002e25716de8cd95590b495b673a0a52c1
sebastiaanwezenberg <sebastiaanwezenberg@gmail.com>
2011-05-18 09:31:26 +0000

    Tile.cs

---
+++ Source/KaroTestGUI/KaroXNA/KaroXNA/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace KaroXNA
+{
+    class Tile
+    {
+        public Model TileModel { get; set; }
+        public Matrix TileMatrix { get; set; }
+        public bool IsMovable { get; set; }
+
+        public Tile(Model model, bool isMovable)
+        {
+            TileModel = model;
+            IsMovable = isMovable;
+        }
+    }
+}
