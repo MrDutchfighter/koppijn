b87efa9ec73e2f7aad55c42153cce941d00db285
sebastiaanwezenberg@gmail.com <sebastiaanwezenberg@gmail.com@fb384490-38b7-be2d-62f0-9a43054d88e4>
2011-05-25 07:45:57 +0000

    tiles en pieces omgezet naar drawablegamecomponents

--- trunk/Source/KaroTestGUI/KaroXNA/KaroXNA/Game1.cs
+++ trunk/Source/KaroTestGUI/KaroXNA/KaroXNA/Game1.cs
@@ -21,17 +21,17 @@ namespace KaroXNA
     {
         GraphicsDeviceManager graphics;
         GraphicsDevice device;
-        Model tileModel, pieceModel;
-        Camera cam;
+        public Model tileModel, pieceModel, tableModel;
+        Matrix tableMatrix;
+        public Camera cam;
         Matrix world, view, proj;
-        KaroEngine.KaroEngineWrapper engine;
-        List<Piece> gamePieces;
-        List<Tile> gameTiles;
+        public KaroEngine.KaroEngineWrapper engine;
         Menu gameMenu;
+        public int[] move;
         GameState gameState;
-        int insertionCount = 0;
+        public int insertionCount = 0;
         MouseState oldMouseState;
-        const int BOARDWIDTH = 17;
+        public const int BOARDWIDTH = 17;
         bool spacePressed = false;
         Random random = new Random();
         public float frames = 0f;
@@ -52,8 +52,6 @@ namespace KaroXNA
             graphics.ApplyChanges();
             this.Window.Title = "Karo XNA";
             Content.RootDirectory = "Content";
-            gamePieces = new List<Piece>();
-            gameTiles = new List<Tile>();
             cam = new Camera(graphics.GraphicsDevice.Viewport.Width / graphics.GraphicsDevice.Viewport.Height);
             gameState = GameState.MENU;
             gameMenu = new Menu(this, 0);
@@ -77,35 +75,38 @@ namespace KaroXNA
         {
             tileModel = Content.Load<Model>("tile");
             pieceModel = Content.Load<Model>("piece");
+            tableModel = Content.Load<Model>("test");
 
             for (int x = 0; x < BOARDWIDTH; x++)
             {
                 for (int y = 0; y < BOARDWIDTH; y++)
                 {
                     KaroEngine.Tile tile = engine.GetByXY(x, y);
-                    
-                    if (tile != KaroEngine.Tile.BORDER && tile != KaroEngine.Tile.EMPTY) {
-                        Tile t = new Tile(tileModel, false, new Point(x, y));
+
+                    if (tile != KaroEngine.Tile.BORDER && tile != KaroEngine.Tile.EMPTY)
+                    {
+                        Tile t = new Tile(this, tileModel, false, new Point(x, y));
                         t.TileMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(random.Next(0, 4) * 90)) * Matrix.CreateTranslation(new Vector3(x * 5.5f, 0, y * 5.5f));
-                        
-                        gameTiles.Add(t);
 
-                        if(tile == KaroEngine.Tile.WHITEUNMARKED || tile == KaroEngine.Tile.WHITEMARKED || tile == KaroEngine.Tile.REDUNMARKED || tile == KaroEngine.Tile.REDMARKED) {
-                            Piece p = new Piece(pieceModel, true, new Point(x, y), Color.Black.ToVector3());
-                            
-                            if (tile == KaroEngine.Tile.REDUNMARKED || tile == KaroEngine.Tile.REDMARKED)
-                                p.Color = Color.Tomato.ToVector3();
+                        Components.Add(t);
 
-                            if (tile == KaroEngine.Tile.WHITEUNMARKED || tile == KaroEngine.Tile.WHITEMARKED)
-                                p.Color = Color.White.ToVector3();
+                        //if (tile == KaroEngine.Tile.WHITEUNMARKED || tile == KaroEngine.Tile.WHITEMARKED || tile == KaroEngine.Tile.REDUNMARKED || tile == KaroEngine.Tile.REDMARKED)
+                        //{
+                        //    Piece p = new Piece(pieceModel, true, new Point(x, y), Color.Black.ToVector3());
 
-                            if (tile == KaroEngine.Tile.WHITEMARKED || tile == KaroEngine.Tile.REDMARKED)
-                                p.T = Matrix.CreateTranslation(new Vector3(x * 5.5f, 1, y * 5.5f));
-                            else
-                                p.T = Matrix.CreateRotationX(MathHelper.ToRadians(180)) * Matrix.CreateTranslation(new Vector3(x * 5.5f, 3.4f, y * 5.5f));
+                        //    if (tile == KaroEngine.Tile.REDUNMARKED || tile == KaroEngine.Tile.REDMARKED)
+                        //        p.Color = Color.Tomato.ToVector3();
 
-                            gamePieces.Add(p);
-                        }
+                        //    if (tile == KaroEngine.Tile.WHITEUNMARKED || tile == KaroEngine.Tile.WHITEMARKED)
+                        //        p.Color = Color.White.ToVector3();
+
+                        //    if (tile == KaroEngine.Tile.WHITEMARKED || tile == KaroEngine.Tile.REDMARKED)
+                        //        p.T = Matrix.CreateTranslation(new Vector3(x * 5.5f, 1, y * 5.5f));
+                        //    else
+                        //        p.T = Matrix.CreateRotationX(MathHelper.ToRadians(180)) * Matrix.CreateTranslation(new Vector3(x * 5.5f, 3.4f, y * 5.5f));
+
+                        //    gamePieces.Add(p);
+                        //}
                     }
                 }
             }
@@ -118,114 +119,79 @@ namespace KaroXNA
 
         protected override void Update(GameTime gameTime)
         {
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-                this.Exit();
-
-            if (Keyboard.GetState().IsKeyDown(Keys.R) || Keyboard.GetState().IsKeyDown(Keys.Right)) { rotY += 0.1f; cam.DoYRotation(rotY); }
-            if (Keyboard.GetState().IsKeyDown(Keys.Left)) { rotY += 0.1f; cam.DoYRotation(rotY * -1); }
-
-            if (!Keyboard.GetState().IsKeyDown(Keys.R) && !Keyboard.GetState().IsKeyDown(Keys.Right) && !Keyboard.GetState().IsKeyDown(Keys.Left)) { rotY = 0.5f; }
-            if (rotY > 4) { rotY = 4f; }
-
-
-            if (Keyboard.GetState().IsKeyDown(Keys.PageUp) || Keyboard.GetState().IsKeyDown(Keys.Up))
-                cam.DoZoom(-0.01f);
-            if (Keyboard.GetState().IsKeyDown(Keys.PageDown) || Keyboard.GetState().IsKeyDown(Keys.Down))
-                cam.DoZoom(0.01f);
-            if (Mouse.GetState().ScrollWheelValue != oldMouseState.ScrollWheelValue)
-            {
-                float difference = (Mouse.GetState().ScrollWheelValue - oldMouseState.ScrollWheelValue);
-                cam.DoZoom(difference / 1000);
-            }
             if (Keyboard.GetState().IsKeyDown(Keys.Space))
             {
                 if (!spacePressed)
                 {
                     spacePressed = true;
 
-                    int[] move = engine.CalculateComputerMove();
+                    move = engine.CalculateComputerMove();
 
-                    Point positionFrom = new Point(move[0] % BOARDWIDTH, move[0] / BOARDWIDTH);
-                    Point positionTo = new Point(move[1] % BOARDWIDTH, move[1] / BOARDWIDTH);
-                    Point tileFrom = new Point(move[2] % BOARDWIDTH, move[2] / BOARDWIDTH);
-
+                    Point positionTo = new Point(move[1] % Game1.BOARDWIDTH, move[1] / Game1.BOARDWIDTH);
                     
                     if (engine.GetGameState() == KaroEngine.GameState.INSERTION || insertionCount < 12)
                     {
-                            Piece p = new Piece(pieceModel, true, new Point(positionTo.X, positionTo.Y), Color.Black.ToVector3());
-                            
-                            if(engine.GetTurn() == KaroEngine.Player.RED)
-                                p.Color = Color.Tomato.ToVector3();
-                            else
-                                p.Color = Color.White.ToVector3();
-                            //Turn the piece upside down, default is flipped, which we don't want!
-                            p.T = Matrix.Identity;
-                            p.T *= Matrix.CreateRotationX(MathHelper.ToRadians(180)) * Matrix.CreateTranslation(new Vector3(positionTo.X * 5.5f, 3.4f, positionTo.Y * 5.5f));
+                        Piece p = new Piece(this, pieceModel, true, new Point(positionTo.X, positionTo.Y), Color.Black.ToVector3());
 
-                            gamePieces.Add(p);
-                            insertionCount++;
-                    }
-                    else
-                    {
-                        foreach (Piece p in gamePieces)
-                        {
-                            if (p.Location == positionFrom)
-                            {
-                                p.T = Matrix.Identity;
+                        if (engine.GetTurn() == KaroEngine.Player.RED)
+                            p.Color = Color.Tomato.ToVector3();
+                        else
+                            p.Color = Color.White.ToVector3();
 
-                                if (!p.IsFlipped)
-                                    p.T *= Matrix.CreateRotationX(MathHelper.ToRadians(180));
+                        //Turn the piece upside down, default is flipped, which we don't want!
+                        p.T = Matrix.Identity;
+                        p.T *= Matrix.CreateRotationX(MathHelper.ToRadians(180)) * Matrix.CreateTranslation(new Vector3(positionTo.X * 5.5f, 3.4f, positionTo.Y * 5.5f));
 
-
-                                if (move[3] == 1)
-                                {
-                                    p.T *= Matrix.CreateRotationX(MathHelper.ToRadians(180));
-                                    p.T *= Matrix.CreateTranslation(new Vector3((positionTo.X) * 5.5f, 1f, (positionTo.Y) * 5.5f));
-
-                                    if (p.IsFlipped)
-                                        p.IsFlipped = false;
-                                    else
-                                        p.IsFlipped = true;
-                                }
-                                else
-                                {
-                                    p.T *= Matrix.CreateTranslation(new Vector3((positionTo.X) * 5.5f, 1f, (positionTo.Y) * 5.5f));
-                                }
-
-                                p.Location = positionTo;
-                            }
-                        }
-                    }
-                    foreach (Tile t in gameTiles)
-                    {
-                        if (t.Location == tileFrom)
-                        {
-                            t.TileMatrix *= Matrix.CreateTranslation(new Vector3((positionTo.X - t.Location.X) * 5.5f, 0, (positionTo.Y - t.Location.Y) * 5.5f));
-                            t.Location = positionTo;
-                        }
+                        Components.Add(p);
+                        insertionCount++;
                     }
                 }
             }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.R) || Keyboard.GetState().IsKeyDown(Keys.Right)) 
+                rotY += 0.1f; cam.DoYRotation(rotY);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) 
+                rotY += 0.1f; cam.DoYRotation(rotY * -1);
+
+            if (!Keyboard.GetState().IsKeyDown(Keys.R) && !Keyboard.GetState().IsKeyDown(Keys.Right) && !Keyboard.GetState().IsKeyDown(Keys.Left)) 
+                rotY = 0.5f;
+
+            if (rotY > 4) 
+                rotY = 4f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.PageUp) || Keyboard.GetState().IsKeyDown(Keys.Up))
+                cam.DoZoom(-0.01f);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.PageDown) || Keyboard.GetState().IsKeyDown(Keys.Down))
+                cam.DoZoom(0.01f);
+
+            if (Mouse.GetState().ScrollWheelValue != oldMouseState.ScrollWheelValue)
+            {
+                float difference = (Mouse.GetState().ScrollWheelValue - oldMouseState.ScrollWheelValue);
+                cam.DoZoom(difference / 1000);
+            }
+
             if (Keyboard.GetState().IsKeyUp(Keys.Space))
             {
                 spacePressed = false;
             }
-            oldMouseState = Mouse.GetState();
 
-            //world = Matrix.CreateRotationY(MathHelper.ToDegrees(f));
-            // TODO: Add your update logic here
+            oldMouseState = Mouse.GetState();
 
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             float fps = 1 / elapsed;
             deltaFPSTime += elapsed;
+
             if (deltaFPSTime > 1)
             {
                 this.FPS = fps;
                 deltaFPSTime -= 1;
             }
-
 
             base.Update(gameTime);
         }
@@ -233,35 +199,6 @@ namespace KaroXNA
         protected override void Draw(GameTime gameTime)
         {
             graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            foreach (Tile t in gameTiles)
-            {
-                foreach (ModelMesh mesh in t.TileModel.Meshes)
-                {
-                    foreach (BasicEffect e in mesh.Effects)
-                    {
-                        e.EnableDefaultLighting();
-
-                        e.World = t.TileMatrix;
-                        e.View = cam.View;
-                        e.Projection = cam.Projection;
-
-                        e.SpecularColor = Color.Black.ToVector3();
-                    }
-
-                    mesh.Draw();
-                }
-            }
-
-            foreach (Piece p in gamePieces)
-            {
-                if (!p.IsVisible)
-                {
-                    continue;
-                }
-
-                p.Draw(cam);
-            }
 
             base.Draw(gameTime);
         }
--- trunk/Source/KaroTestGUI/KaroXNA/KaroXNA/Piece.cs
+++ trunk/Source/KaroTestGUI/KaroXNA/KaroXNA/Piece.cs
@@ -1,57 +1,124 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
 
 namespace KaroXNA
 {
     using XNAColor = Microsoft.Xna.Framework.Color;
-
-    class Piece
+        
+    public class Piece : Microsoft.Xna.Framework.DrawableGameComponent
     {
+        public Game1 game;
+
         public Model PieceModel { get; set; }
-        public Matrix PieceMatrix { get; set; }
         public bool IsVisible { get; set; }
-        public bool IsFlipped { get; set; }
         public Point Location { get; set; }
         public Vector3 Color { get; set; }
+
+        public Matrix PieceMatrix { get; set; }
         public Matrix T { get; set; }
-        
-        public Piece(Model model, bool visible, Point location, Vector3 color)
+
+        public bool IsFlipped { get; set; }
+        bool spacePressed;
+
+        public Piece(Game game, Model pieceModel, bool visible, Point location, Vector3 color)
+            : base(game)
         {
-            PieceModel = model;
+            this.game = (Game1)game;
+
+            PieceModel = pieceModel;
             IsVisible = visible;
             Location = location;
             Color = color;
+
             PieceMatrix = Matrix.Identity;
             T = Matrix.Identity;
+
             IsFlipped = false;
+            spacePressed = false;
         }
 
-        public void Draw(Camera cam)
+        public override void Initialize()
         {
-            foreach (ModelMesh mesh in PieceModel.Meshes)
-            {
-                foreach (BasicEffect e in mesh.Effects)
-                {
-                    e.PreferPerPixelLighting = true;
-
-                    e.DiffuseColor = Color;
-                    e.SpecularColor = XNAColor.White.ToVector3();
-                    e.EnableDefaultLighting();
-                    e.World = PieceMatrix * T;
-                    e.View = cam.View;
-                    e.Projection = cam.Projection;
-                }
-
-                mesh.Draw();
-            }
+            base.Initialize();
         }
 
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                if (!spacePressed)
+                {
+                    spacePressed = true;
 
+                    Point positionFrom = new Point(game.move[0] % Game1.BOARDWIDTH, game.move[0] / Game1.BOARDWIDTH);
+                    Point positionTo = new Point(game.move[1] % Game1.BOARDWIDTH, game.move[1] / Game1.BOARDWIDTH);
+                    Point tileFrom = new Point(game.move[2] % Game1.BOARDWIDTH, game.move[2] / Game1.BOARDWIDTH);
+
+
+                    if (game.engine.GetGameState() != KaroEngine.GameState.INSERTION || game.insertionCount > 12)
+                    {
+                        if (Location == positionFrom)
+                        {
+                            T = Matrix.Identity;
+
+                            if (!IsFlipped)
+                                T *= Matrix.CreateRotationX(MathHelper.ToRadians(180));
+
+                            if (game.move[3] == 1)
+                            {
+                                T *= Matrix.CreateRotationX(MathHelper.ToRadians(180));
+                                T *= Matrix.CreateTranslation(new Vector3((positionTo.X) * 5.5f, 1f, (positionTo.Y) * 5.5f));
+
+                                if (IsFlipped)
+                                    IsFlipped = false;
+                                else
+                                    IsFlipped = true;
+                            }
+                            else
+                            {
+                                T *= Matrix.CreateTranslation(new Vector3((positionTo.X) * 5.5f, 1f, (positionTo.Y) * 5.5f));
+                            }
+
+                            Location = positionTo;
+                        }
+                    }
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (Visible)
+            {
+                foreach (ModelMesh mesh in PieceModel.Meshes)
+                {
+                    foreach (BasicEffect e in mesh.Effects)
+                    {
+                        e.EnableDefaultLighting();
+                        e.PreferPerPixelLighting = true;
+                        e.DiffuseColor = Color;
+                        
+                        e.World = PieceMatrix * T;
+                        e.View = game.cam.View;
+                        e.Projection = game.cam.Projection;
+                    }
+
+                    mesh.Draw();
+                }
+            }
+
+            base.Draw(gameTime);
+        }
     }
 }
