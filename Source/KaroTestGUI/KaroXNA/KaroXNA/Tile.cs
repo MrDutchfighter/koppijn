aedb0510a224eb5f67f68ce1428b5759df0c144b
sebastiaanwezenberg <sebastiaanwezenberg@gmail.com>
2011-05-25 07:45:57 +0000

    tiles en pieces omgezet naar drawablegamecomponents

--- Source/KaroTestGUI/KaroXNA/KaroXNA/Tile.cs
+++ Source/KaroTestGUI/KaroXNA/KaroXNA/Tile.cs
@@ -1,25 +1,85 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
 
 namespace KaroXNA
 {
-    class Tile
+    public class Tile : Microsoft.Xna.Framework.DrawableGameComponent
     {
+        public Game1 game;
         public Model TileModel { get; set; }
         public Matrix TileMatrix { get; set; }
         public bool IsMovable { get; set; }
-        
-        public Point Location { get; set; }
 
-        public Tile(Model model, bool isMovable, Point location)
+        public Point Location { get; set; }
+        bool spacePressed = false;
+
+        public Tile(Game game, Model model, bool isMovable, Point location)
+            : base(game)
         {
+            this.game = (Game1)game;
             TileModel = model;
             IsMovable = isMovable;
             Location = location;
+        }
+
+        public override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                if (!spacePressed)
+                {
+                    spacePressed = true;
+                    
+                    Point positionFrom = new Point(game.move[0] % Game1.BOARDWIDTH, game.move[0] / Game1.BOARDWIDTH);
+                    Point positionTo = new Point(game.move[1] % Game1.BOARDWIDTH, game.move[1] / Game1.BOARDWIDTH);
+                    Point tileFrom = new Point(game.move[2] % Game1.BOARDWIDTH, game.move[2] / Game1.BOARDWIDTH);
+                    
+                    if (game.engine.GetGameState() != KaroEngine.GameState.INSERTION || game.insertionCount > 12)
+                    {
+                        if (Location == tileFrom)
+                        {
+                            TileMatrix *= Matrix.CreateTranslation(new Vector3((positionTo.X - Location.X) * 5.5f, 0, (positionTo.Y - Location.Y) * 5.5f));
+                            Location = positionTo;
+                        }
+                    }
+                }
+            }
+            
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (ModelMesh mesh in TileModel.Meshes)
+            {
+                foreach (BasicEffect e in mesh.Effects)
+                {
+                    e.EnableDefaultLighting();
+
+                    e.World = TileMatrix;
+                    e.View = game.cam.View;
+                    e.Projection = game.cam.Projection;
+                }
+
+                mesh.Draw();
+            }
+            
+            base.Draw(gameTime);
         }
     }
 }
