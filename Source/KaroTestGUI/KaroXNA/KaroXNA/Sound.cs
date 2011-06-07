eada6d27a4d4adac0bcfaf11e13faa1df7e4ed00
lucas.pekel <lucas.pekel@gmail.com>
2011-06-07 10:07:41 +0000

    [ en ] laten de radio nu van zender verranderen

--- Source/KaroTestGUI/KaroXNA/KaroXNA/Sound.cs
+++ Source/KaroTestGUI/KaroXNA/KaroXNA/Sound.cs
@@ -17,13 +17,15 @@ namespace KaroXNA
     public class Sound {
 
         private static Sound instance;
-
+        private int rotate;
         /// <summary>
         /// the sound constructor
         /// </summary>
         /// <param name="game"></param>
-        private Sound(Game game) {
+        private Sound() {
             player.URL = "http://livestreams.omroep.nl/npo/3fm-bb"; //3fm
+            player.controls.stop();
+            rotate = 0;
         }
 
         static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
@@ -54,6 +56,34 @@ namespace KaroXNA
             player.controls.stop();
         }
 
+        private void Check() {
+            switch (rotate) {
+                case 1: this.Play3FM(); break;
+                case 2: this.PlayQMusic(); break;
+                default :
+                    this.rotate=0;
+                    this.Stop();
+                    break;
+            }
+        }
+
+        public void goLeft() {
+            if (rotate == 0) {
+                this.rotate = 2;
+            }
+            else
+            {
+                this.rotate -= 1;
+            }
+            this.Check();
+        }
+
+        public void goRight() {
+            this.rotate += 1;
+            this.Check();
+        }
+
+
         /// <summary>
         /// Start playing the current URL
         /// </summary>
@@ -66,12 +96,12 @@ namespace KaroXNA
         /// </summary>
         /// <param name="game"></param>
         /// <returns></returns>
-        public static Sound Instance(Game game)
+        public static Sound Instance()
         {
 
                 if (instance == null)
                 {
-                    instance = new Sound(game);
+                    instance = new Sound();
                 }
                 return instance;
         }
